975db4beb1b9bc743ea7674a8228efff4fdcefee
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-08-24 12:53:25 -0400

    Initial commit

---
+++ AccountTest/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    public class Account
+    {
+        private string name;
+        private decimal balance;
+        public string Name {
+            get { return name; }
+
+            set { name = value; }
+        }
+
+        public Account()
+        {
+            balance = 0;
+            Name = "Unknown";
+        }
+       /*
+        *public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string inName)
+        {
+            name = inName;
+        }
+        */
+    }
+}
---
+++ AccountTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+
+            myAccount.SetName("Isabella Bennett");
+
+            Console.WriteLine("The name is:" + myAccount.GetName());
+
+            Account hisAccount = new Account();
+
+            hisAccount.SetName("Isaac Bennett");
+
+            Console.WriteLine("The name is:" + hisAccount.GetName());
+
+            Account theirAccount = new Account();
+
+            theirAccount.SetName("Dr Bennett");
+
+            Console.WriteLine("The name is:" + theirAccount.GetName());
+        }
+    }
+}
